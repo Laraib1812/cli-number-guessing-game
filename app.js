a2bd868c0cfd61d552907a8de9ec20abe9e3fe3f
Laraib1812 <142295360+Laraib1812@users.noreply.github.com>
2024-05-02 14:03:39 +0500

    Adding a number guessing game

---
+++ app.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log('\n\tWelcome to "The number guessing game", by Muhammad Laraib\n');
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "Guess a number between 1 and 5: "
+    }
+]);
+if (answer.userGuess === randomNumber) {
+    console.log("You guessed it right!");
+}
+else {
+    console.log("You guessed it wrong! please try again");
+}
+;
